6f1b385508b7be2e2ffae7a403e5c9e1ec413649
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-21 18:10:47 -0400

    update

---
+++ owproject/src/main/java/com/ourowproject/owproject/OwprojectApplication.java
@@ -0,0 +1,12 @@
+package com.ourowproject.owproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwprojectApplication.class, args);
+	}
+}
