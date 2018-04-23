6d75094f5684e4fb8b08e15dee53e71a04e29d02
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-04-23 11:15:11 -0400

    logo

--- owproject/src/main/java/com/ourowproject/owproject/OwprojectApplication.java
+++ owproject/src/main/java/com/ourowproject/owproject/OwprojectApplication.java
@@ -1,12 +1,27 @@
 package com.ourowproject.owproject;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.PrintStream;
 
 @SpringBootApplication
 public class OwprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OwprojectApplication.class, args);
+//		SpringApplication.run(OwprojectApplication.class, args);
+
+		SpringApplication app = new SpringApplication(SpringBootApplication.class);
+		app.setBanner((environment, sourceClass, out) -> out.print(" _____  _    _  ______             _              ______          _           _   \n" +
+                "|  _  || |  | | | ___ \\           (_)             | ___ \\        (_)         | |  \n" +
+                "| | | || |  | | | |_/ /_ _ ___ ___ _  ___  _ __   | |_/ / __ ___  _  ___  ___| |_ \n" +
+                "| | | || |/\\| | |  __/ _` / __/ __| |/ _ \\| '_ \\  |  __/ '__/ _ \\| |/ _ \\/ __| __|\n" +
+                "\\ \\_/ /\\  /\\  / | | | (_| \\__ \\__ \\ | (_) | | | | | |  | | | (_) | |  __/ (__| |_ \n" +
+                " \\___/  \\/  \\/  \\_|  \\__,_|___/___/_|\\___/|_| |_| \\_|  |_|  \\___/| |\\___|\\___|\\__|\n" +
+                "                                                                _/ |              \n" +
+                "                                                               |__/               "));
+		app.run(args);
 	}
 }
