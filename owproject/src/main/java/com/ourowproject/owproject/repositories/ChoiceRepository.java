424ac15595c07a3e46dfc28f0438291c03229299
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-23 13:20:05 -0400

    fully finish match service, partially finish choice service

--- owproject/src/main/java/com/ourowproject/owproject/repositories/ChoiceRepository.java
+++ owproject/src/main/java/com/ourowproject/owproject/repositories/ChoiceRepository.java
@@ -1,8 +1,12 @@
 package com.ourowproject.owproject.repositories;
 
 import com.ourowproject.owproject.entities.Choice;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface ChoiceRepository extends CrudRepository<Choice, Long>{
+//    Iterable<Choice> getAllChoicesByMatch_id(Long match_id);
+//    Iterable<Choice> getAllChoicesByUser_id(Long user_id);
+    Choice findChoiceById(Long choiceId);
 
 }
--- owproject/src/main/java/com/ourowproject/owproject/services/ChoiceService.java
+++ owproject/src/main/java/com/ourowproject/owproject/services/ChoiceService.java
@@ -1,10 +1,51 @@
 package com.ourowproject.owproject.services;
 
+import com.ourowproject.owproject.entities.Choice;
 import com.ourowproject.owproject.entities.User;
+import com.ourowproject.owproject.repositories.ChoiceRepository;
+import com.ourowproject.owproject.repositories.MatchRepository;
+import com.ourowproject.owproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChoiceService {
 
+    private ChoiceRepository choiceRepository;
+    private MatchRepository matchRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public ChoiceService(ChoiceRepository choiceRepository, MatchRepository matchRepository, UserRepository userRepository) {
+        this.choiceRepository = choiceRepository;
+        this.matchRepository = matchRepository;
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<Iterable<Choice>> getAllChoices() {
+        return new ResponseEntity<>(choiceRepository.findAll(), HttpStatus.OK);
+    }
+
+//    public ResponseEntity<Iterable<Choice>> getAllChoicesByMatch_id(Long match_id) {
+//        return new ResponseEntity<>(choiceRepository.getAllChoicesByMatch_id(match_id), HttpStatus.OK);
+//    }
+
+//    public ResponseEntity<Iterable<Choice>> getAllChoicesByUser_id(Long userId) {
+//        return new
+//    }
+
+
+    public ResponseEntity<Choice> getChoiceById(Long choiceId) {
+        return new ResponseEntity<>(choiceRepository.findChoiceById(choiceId), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Choice> createChoice(Choice choice) {
+        return new ResponseEntity<>(choiceRepository.save(choice), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Choice> updateChoice(Choice choice) {
+        return new ResponseEntity<>(choiceRepository.save(choice), HttpStatus.OK);
+    }
 }
