01533f47426b4ffcb4545ff49ae1351f9008fec5
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-23 11:33:59 -0400

    add service annotations

--- owproject/src/main/java/com/ourowproject/owproject/services/ChoiceService.java
+++ owproject/src/main/java/com/ourowproject/owproject/services/ChoiceService.java
@@ -2,7 +2,9 @@ package com.ourowproject.owproject.services;
 
 import com.ourowproject.owproject.entities.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ChoiceService {
 
 }
