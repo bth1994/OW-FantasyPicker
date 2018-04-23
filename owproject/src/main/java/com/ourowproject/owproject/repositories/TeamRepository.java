662bfab67d98515b9152f10de0fbb8f3eb43bdae
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-23 11:50:09 -0400

    finish team service

--- owproject/src/main/java/com/ourowproject/owproject/repositories/TeamRepository.java
+++ owproject/src/main/java/com/ourowproject/owproject/repositories/TeamRepository.java
@@ -4,4 +4,6 @@ import com.ourowproject.owproject.entities.Team;
 import org.springframework.data.repository.CrudRepository;
 
 public interface TeamRepository extends CrudRepository<Team, Long>{
+
+    Team findTeamById(Long teamId);
 }
--- owproject/src/main/java/com/ourowproject/owproject/services/TeamService.java
+++ owproject/src/main/java/com/ourowproject/owproject/services/TeamService.java
@@ -1,7 +1,35 @@
 package com.ourowproject.owproject.services;
 
+import com.ourowproject.owproject.entities.Team;
+import com.ourowproject.owproject.repositories.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
+
+    private TeamRepository teamRepository;
+
+    @Autowired
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public ResponseEntity<Iterable<Team>> getAllTeams() {
+        return new ResponseEntity<>(teamRepository.findAll(), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Team> getTeamById(Long teamId) {
+        return new ResponseEntity<>(teamRepository.findTeamById(teamId), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Team> createTeam(Team team) {
+        return new ResponseEntity<>(teamRepository.save(team), HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<Team> updateTeam(Team team) {
+        return new ResponseEntity<>(teamRepository.save(team), HttpStatus.OK);
+    }
 }
